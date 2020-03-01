03a1b50d838f7ef8118775d0ac1ea39a27d6a65e
George Lesica <george@lesica.com>
2020-03-01 08:40:14 -0700

    Add version test file, oops

---
+++ tests/version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "testing version agreement"
+claimed=$(pyrs990 --version)
+actual=$(poetry version | cut -d ' ' -f 2)
+if [ "$claimed" !=  "$actual" ]; then
+  echo "$claimed (claimed version) and $actual (actual version) do not match"
+  exit 1
+else
+  echo "version verified"
+fi
